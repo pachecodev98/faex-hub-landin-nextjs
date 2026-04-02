e62b795d3ecb90504771af827a33a63295fe1fb9
Natã Pacheco <natan.pacheco.1998@gmail.com>
2026-04-01 23:19:25 -0300

    alteracoes em page tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import cabecalho from "@/components/Header";
-import hero from "@/components/Hero";
-import funcionalidades_secao from "@/components/Features";
-import depoimentos_secao from "@/components/Testimonials";
-import chamada_acao from "@/components/CTA";
-import rodape from "@/components/Footer";
+import Cabecalho from "@/components/Header";
+import Hero from "@/components/Hero";
+import FuncionalidadesSecao from "@/components/Features";
+import DepoimentosSecao from "@/components/Testimonials";
+import ChamadaAcao from "@/components/CTA";
+import Rodape from "@/components/Footer";
 
-export default function pagina_inicial(): JSX.Element {
+export default function PaginaInicial(): JSX.Element {
   return (
     <main>
-      <cabecalho />
-      <hero />
-      <funcionalidades_secao />
-      <depoimentos_secao />
-      <chamada_acao />
-      <rodape />
+      <Cabecalho />
+      <Hero />
+      <FuncionalidadesSecao />
+      <DepoimentosSecao />
+      <ChamadaAcao />
+      <Rodape />
     </main>
   );
 }
